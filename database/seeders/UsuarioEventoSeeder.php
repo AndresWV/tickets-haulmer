8494050f06a237450e8a3329b33e81d56d3b4902
AndresWV <andres.wv@hotmail.com>
2023-10-03 09:35:22 -0300

    data usuarios, ok

--- database/seeders/UsuarioEventoSeeder.php
+++ database/seeders/UsuarioEventoSeeder.php
@@ -2,16 +2,51 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
 
 class UsuarioEventoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $usuarios = [
+            [
+                'nombre' => 'Usuario1',
+                'apellido' => 'Apellido1',
+                'mail' => 'usuario1@example.com',
+                'password' => Hash::make('contraseña1'),
+            ],
+            [
+                'nombre' => 'Usuario2',
+                'apellido' => 'Apellido2',
+                'mail' => 'usuario2@example.com',
+                'password' => Hash::make('contraseña2'),
+            ],
+            [
+                'nombre' => 'Usuario3',
+                'apellido' => 'Apellido3',
+                'mail' => 'usuario3@example.com',
+                'password' => Hash::make('contraseña3'),
+            ],
+            [
+                'nombre' => 'Usuario4',
+                'apellido' => 'Apellido4',
+                'mail' => 'usuario4@example.com',
+                'password' => Hash::make('contraseña4'),
+            ],
+            [
+                'nombre' => 'Usuario5',
+                'apellido' => 'Apellido5',
+                'mail' => 'usuario5@example.com',
+                'password' => Hash::make('contraseña5'),
+            ],
+        ];
+
+        DB::table('usuario_eventos')->insert($usuarios);
     }
 }
