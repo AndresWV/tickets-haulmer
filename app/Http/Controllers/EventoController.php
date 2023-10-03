efa89b941c7cbaa3c7e1fa134a315d73ccc4edab
AndresWV <andres.wv@hotmail.com>
2023-10-03 17:06:29 -0300

    EP getUsers + EP crear event

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -33,4 +33,35 @@ class EventoController extends Controller
             ], 500);
         }
     }
+    public function store(Request $request)
+    {
+        try {
+            $data = $request->all();
+
+            // Validación de datos (puedes personalizar las reglas de validación según tus necesidades)
+            $validatedData = $this->validate($request, [
+                'nombre_evento' => 'required|string',
+                'fecha' => 'required|date',
+                'hora' => 'required|string',
+                'lugar' => 'required|string',
+                'precio' => 'required|numeric',
+                'disponibilidad' => 'required|integer',
+                'categoria' => 'required|string',
+                'descripcion' => 'required|string',
+                'url_entradas' => 'required|string|url',
+            ]);
+
+            // Crear un nuevo evento
+            $evento = Evento::create($validatedData);
+
+            return response()->json(['message' => 'Evento creado con éxito', 'data' => $evento], 201);
+        } catch (Throwable $e) {
+            error_log($e->getMessage());
+            return response([
+                'message' => 'Error interno del servidor',
+                'code' => 500,
+                'data' => [],
+            ], 500);
+        }
+    }
 }
--- app/Http/Controllers/UsuarioEventoController.php
+++ app/Http/Controllers/UsuarioEventoController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UsuarioEvento;
 use Throwable;
 class UsuarioEventoController extends Controller
 {
-    //
+    public function index()
+    {
+        try{
+            $users = UsuarioEvento::all();
+            return response()->json($users);
+        }catch (Throwable $e) {
+            error_log($e->getMessage());
+            return response([
+                'message' => 'server_error',
+                'code' => 500,
+                'data' => []
+            ], 500);
+        }
+    }
 }
